04020b1c8267b869da7786f96740a483dd720141
AbdullahFahim1 <fahimsk708@gmail.com>
2025-05-21 00:48:18 +0600

    first commit

---
+++ Project.js
@@ -0,0 +1,161 @@
+const startBtn = document.querySelector(".start-btn button");
+const rule = document.querySelector(".rule");
+const exit = document.querySelector(".btns .exit");
+const continu = document.querySelector(".btns .continue");
+const question = document.querySelector(".question-1");
+const nextbtn = document.querySelector(".nextbtn");
+const timer = document.querySelector(".timer .second");
+const timeliner = document.querySelector(".questionHeader .timeline")
+const result = document.querySelector(".result");
+const replaybtn = document.querySelector(".buttons .replay");
+const quitbtn = document.querySelector(".buttons .quit");
+
+startBtn.onclick = ()=>{
+    rule.classList.add("activeRule")
+}
+exit.onclick = ()=>{
+    rule.classList.remove("activeRule")
+}
+continu.onclick = ()=>{
+    question.classList.add("startQuiz");
+    startTimer(15)
+    startTimerLine(0)
+    ShowQuestion(0)
+}
+
+
+let quesCount = 0;
+let counter;
+let timeValue = 15;
+let counterLine;
+let widthValue = 0;
+let userScore = 0;
+nextbtn.onclick = ()=>{
+    
+    if(quesCount <ques.length - 1){
+        quesCount ++
+        clearInterval(counter);
+        startTimer(timeValue);
+
+        clearInterval(counterLine);
+        startTimerLine(widthValue);
+        nextbtn.style.display = "none";
+        ShowQuestion(quesCount);
+        
+
+    }else{
+        
+        console.log("You have complete Your Task ");
+        showREsult()
+    }
+}
+
+function ShowQuestion(index){
+    const span = document.querySelector(".span");
+    let quesOption = document.querySelector(".ques-1")
+    let optionTag = `<div class="ques-1-option">` + ques[index].options[0] + `</div>` +
+                    `<div class="ques-1-option">` + ques[index].options[1] + `</div>`+
+                    `<div class="ques-1-option">` + ques[index].options[2] + `</div>`+
+                    `<div class="ques-1-option">` + ques[index].options[3] + `</div>`
+    let quesTag = "<span>" + ques[index].num + ". " + ques[index].question + "</span>"
+    span.innerHTML = quesTag;
+    quesOption.innerHTML = optionTag;
+    const total_Ques = document.querySelector(".total-Ques");
+    let total_Ques_tag ="<p>" + ques[index].num + ' of '+ ques.length +' Question' + "</p>";
+    total_Ques.innerHTML = total_Ques_tag;
+
+
+    const option = quesOption.querySelectorAll(".ques-1-option");
+    for(i=0; 1 < option.length; i++){
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
+
+    
+let tickIcon = '<div class="tick icon"><i class="fas fa-check"></i></div>';
+let crossIcon = '<div class="cross icon"><i class="fas fa-times"></i></div>';
+function optionSelected(answer){
+    clearInterval(counter)
+    clearInterval(counterLine)
+    let userAns = answer.textContent;
+    let correctAns = ques[quesCount].answer;
+    let quesOption = document.querySelector(".ques-1");
+    let activeOption = quesOption.children.length;
+    if(userAns == correctAns){
+        userScore += 1;
+        console.log(userScore);
+        answer.classList.add("correct");
+        console.log("Correct");
+        answer.insertAdjacentHTML("beforeend", tickIcon);
+    }else{
+        answer.classList.add("wrong")
+        console.log("Discorrect")
+        answer.insertAdjacentHTML("beforeend", crossIcon);
+        for(let i=0; i<activeOption; i++){
+            if(quesOption.children[i].textContent == correctAns){
+                quesOption.children[i].setAttribute("class", "ques-1-option correct");
+                quesOption.children[i].insertAdjacentHTML("beforeend", tickIcon);
+            }
+        }
+    }
+    for(let i=0; i<activeOption; i++){
+        quesOption.children[i].classList.add("deactive")
+    }
+        nextbtn.style.display = "block";
+}
+
+
+function showREsult() {
+    rule.classList.remove("activeRule");
+    question.classList.remove("startQuiz");
+    result.classList.add("activeReult");
+
+    const scoreText = document.querySelector(".scoreText");
+    if(userScore > 3){
+        const scoreTag = '<p>Congratulation 😍 You Got <span>' + userScore + '</span> Out of <span>' + ques.length + '</span></p>';
+        scoreText.innerHTML = scoreTag;
+    }
+    else if(userScore > 1){
+        const scoreTag = '<p>Carry On 😃 You Got <span>' + userScore + '</span> Out of <span>' + ques.length + '</span></p>';
+        scoreText.innerHTML = scoreTag;
+    }
+    else{
+        const scoreTag = '<p>Sorry 😂 You Got <span>' + userScore + '</span> Out of <span>' + ques.length + '</span></p>';
+        scoreText.innerHTML = scoreTag;
+    }
+}
+
+function startTimer(time){
+    counter = setInterval(timers, 1000);
+    function timers(){
+        timer.textContent = time;
+        time--
+        if(time < 9){
+            let addZero = timer.textContent;
+            timer.textContent = 0 + addZero;
+        }
+        if(time<0){
+            clearInterval(counter);
+            timer.textContent = "00"
+        }
+    }
+}
+function startTimerLine(time){
+        counterLine = setInterval(timeLine, 50);
+        function timeLine(){
+            time  += 1;
+            timeliner.style.width = time + "px";
+            if(time > 349){
+                clearInterval(counterLine);
+            }
+        }
+}
+quitbtn.onclick = ()=>{
+    window.location.reload();
+}
+replaybtn.onclick = ()=>{
+    rule.classList.add("activeRule")
+
+    result.classList.remove("activeReult");
+
+}
---
+++ ProjectQues.js
@@ -0,0 +1,108 @@
+let ques = [
+    {
+        num: 1,
+        question: "Who is the main Villain in Naruto ?",
+        answer: "Black Jetsu",
+        options: [
+            "Madara Uchiha",
+            "Black Jetsu",
+            "Pain",
+            "Sasuke"
+        ]
+    },
+    {
+        num: 2,
+        question: "Who is the Master Minder in Bleach ?",
+        answer: "Aizen",
+        options: [
+            "Ichigo",
+            "Yhwach",
+            "Rukia",
+            "Aizen"
+        ]
+    },
+    {
+        num: 3,
+        question: "Which Character Sacrifice his family in Naruto ?",
+        answer: "Itachi Uchiha",
+        options: [
+            "Itachi Uchiha",
+            "Naruto",
+            "Sasuke Uchiha",
+            "Hinata Hyuga"
+        ]
+    },
+    {
+        num: 4,
+        question: "What is best & Power Spell of all time ?",
+        answer: "Telipotation(No Catch Ur Speed)",
+        options: [
+            "Gravity(Grave Anything)",
+            "Telipotation(No Catch Ur Speed)",
+            "Helling(Anything Can Hell)",
+            "Summoning(Familiar Beast)"
+        ]
+    },
+    {
+        num: 5,
+        question: "Which is The Most Powerful Jutsu in Naruto ?",
+        answer: "Infinite Tsukuyomi.",
+        options: [
+            "Tailed Beast Ball Rasenshuriken.",
+            " Indra's Arrow.",
+            "Infinite Tsukuyomi.",
+            "Edo Tensei."
+        ]
+    },
+    {
+        num: 6,
+        question: "Most Powerful Bankai in Bleach ?",
+        answer: "Aizen's Bankai (Unnamed)",
+        options: [
+            "Yamamoto's Zanka no Tachi",
+            "Aizen's Bankai (Unnamed)",
+            "Katen Kyokotsu Karamatsu Shinju",
+            "Satsu Karagara Shigarami No Suj"
+        ]
+    },
+    {    num: 7,
+        question: "What is the hair color of Naruto ?",
+        answer: "Yellow",
+        options: [
+            "Black",
+            "Yellow",
+            "Brown",
+            "Orange"
+        ]
+    },
+    {    num: 8,
+        question: "How many leaf in Asta's Grimoire ?",
+        answer: "5",
+        options: [
+            "6",
+            "4",
+            "5",
+            "3"
+        ]
+    },
+    {    num: 9,
+        question: "What was most Ghoul want ?",
+        answer: "Human Flesh",
+        options: [
+            "Money",
+            "Power",
+            "Peace",
+            "Human Flesh"
+        ]
+    },
+    {    num: 10,
+        question: "Remove one Anime in the list ?",
+        answer: "I Can't 🐸",
+        options: [
+            "Naruto",
+            "Demon Slayer",
+            "Death Note",
+            "I Can't 🐸"
+        ]
+    },
+]
